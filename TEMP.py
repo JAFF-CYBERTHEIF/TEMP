f723bc32e5c421a37a9d89334d4bb528746fe596
JAWAHAR <63846848+JAFF-CYBERTHEIF@users.noreply.github.com>
2023-03-25 10:27:19 +0530

    Update TEMP.py

--- TEMP.py
+++ TEMP.py
@@ -1,27 +1,40 @@
-import cv2
-from flask import Flask, Response
+import RPi.GPIO as GPIO
+import time
+import paramiko
 
-app = Flask(__name__)
+# Set up the GPIO pin for the servo
+servo_pin = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servo_pin, GPIO.OUT)
+servo_pwm = GPIO.PWM(servo_pin, 50) # 50Hz PWM frequency
+servo_pwm.start(0) # Set the initial duty cycle to 0
 
-# Function to generate video frames
-def generate_frames():
-    cap = cv2.VideoCapture(0)
-    while True:
-        success, frame = cap.read()
-        if not success:
-            break
-        else:
-            # Encode the frame as a JPEG image
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            # Yield the frame in the response
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-            
-# Route to start the video stream
-@app.route('/video_feed')
-def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+# Connect to the SSH server on the computer
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.connect('your_computer_ip_address', username='your_username', password='your_password')
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+# Define a function to map the joystick input to the servo duty cycle
+def map_servo(value):
+    # Map the joystick input range (0 to 255) to the servo duty cycle range (2 to 12)
+    return (value / 255.0) * 10.0 + 2.0
+
+# Loop to read the joystick inputs over SSH and control the servo
+while True:
+    # Read the joystick inputs over SSH
+    stdin, stdout, stderr = ssh.exec_command('cat /dev/input/js0 | xxd -p -c 8')
+
+    # Parse the joystick input values from the output of the SSH command
+    data = stdout.readline().strip()
+    if len(data) == 16:
+        x = int(data[6:8] + data[4:6], 16) - 32768
+        y = int(data[10:12] + data[8:10], 16) - 32768
+
+        # Map the joystick inputs to the servo duty cycle
+        duty_cycle = map_servo(x)
+
+        # Set the servo duty cycle
+        servo_pwm.ChangeDutyCycle(duty_cycle)
+
+        # Wait for a short time to prevent jitter in the servo movement
+        time.sleep(0.01)
