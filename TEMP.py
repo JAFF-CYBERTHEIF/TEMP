ef91becade0bc6a430b739f60e6dc0596a4d9095
JAWAHAR <63846848+JAFF-CYBERTHEIF@users.noreply.github.com>
2023-03-25 09:44:47 +0530

    Update TEMP.py

--- TEMP.py
+++ TEMP.py
@@ -1,38 +1,27 @@
-import socket
+import cv2
+from flask import Flask, Response
 
-HOST = '0.0.0.0'  # Use '0.0.0.0' to listen on all available network interfaces
-PORT = 1234  # Use any available port number (above 1024)
+app = Flask(__name__)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print(f'Server listening on {HOST}:{PORT}')
-    conn, addr = s.accept()
-    with conn:
-        print(f'Connected by {addr}')
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(f'Received data: {data.decode()}')
-            conn.sendall(data)
-
-            
-            
-            
-            
-            
-import socket
-
-SERVER_IP = '27.62.59.201'  # Replace with the public IP address of the server
-SERVER_PORT = 1234  # Replace with the port number used by the server
-
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.connect((SERVER_IP, SERVER_PORT))
-    print(f'Connected to server at {SERVER_IP}:{SERVER_PORT}')
+# Function to generate video frames
+def generate_frames():
+    cap = cv2.VideoCapture(0)
     while True:
-        message = input('Enter angle 0 - 180 : ')
-        s.sendall(message.encode())
-        data = s.recv(1024)
-        print(f'Received data: {data.decode()}')
+        success, frame = cap.read()
+        if not success:
+            break
+        else:
+            # Encode the frame as a JPEG image
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            # Yield the frame in the response
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+            
+# Route to start the video stream
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
